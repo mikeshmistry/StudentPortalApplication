b7f8b6961753b3041024f234c7efa95c0f85f9ea
Happy user <Happy user@DESKTOP-C8BUOD4>
2021-01-19 22:36:48 -0500

    Added Security Models

---
+++ StudentPortal.UI.Models/SecurityModels/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+namespace StudentPortal.UI.Models.SecurityModels
+{
+    /// <summary>
+    /// Model class for error messages
+    /// </summary>
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
