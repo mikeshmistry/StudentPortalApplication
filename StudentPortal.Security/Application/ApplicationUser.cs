f730d117e36efd3b80ce37e4d5efb34388040837
Happy user <Happy user@DESKTOP-C8BUOD4>
2021-01-19 22:34:54 -0500

    Added Context Classes to Security Library

---
+++ StudentPortal.Security/Application/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace StudentPortal.Security.Application
+{
+    /// <summary>
+    /// Class to represent an user
+    /// </summary>
+    public class ApplicationUser : IdentityUser
+    {
+        
+    }
+}
