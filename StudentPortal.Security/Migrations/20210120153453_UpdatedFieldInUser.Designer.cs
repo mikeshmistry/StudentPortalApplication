099de50059d10bc5bd6273e46e76312c30b72297
Happy user <Happy user@DESKTOP-C8BUOD4>
2021-01-20 22:24:34 -0500

    Updated Database Migration For New StudentId Field In User Table

--- StudentPortal.Security/Migrations/20210120153453_UpdatedFieldInUser.Designer.cs
+++ StudentPortal.Security/Migrations/20210120153453_UpdatedFieldInUser.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StudentPortal.Security.Application;
 
 namespace StudentPortal.Security.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210120153453_UpdatedFieldInUser")]
+    partial class UpdatedFieldInUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ StudentPortal.Security/Migrations/20210120153453_UpdatedFieldInUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentPortal.Security.Migrations
+{
+    public partial class UpdatedFieldInUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StudentId",
+                table: "User",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StudentId",
+                table: "User");
+        }
+    }
+}
